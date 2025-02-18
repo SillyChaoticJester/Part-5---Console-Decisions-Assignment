a478b8311e08fef458f61221221818423b10b864
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-18 13:52:38 -0500

    Working on problem One

--- Program.cs
+++ Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int userAngle, hurricaneSpeed;
+            double minCost, userCost, maxCost;
+            minCost = 4.00;
+            maxCost = 20.00;
+
+            Console.WriteLine("Type down an angle between 0-360, and I'll tell you the direction");
+            userAngle = Convert.ToInt32(Console.ReadLine());
+            if (userAngle == 360 || userAngle == 0)
+            {
+
+                Console.WriteLine("That would be North!");
+
+            }
+            else if (userAngle == 90)
+            {
+
+                Console.WriteLine("That would be East");
+
+            }
+            else if (userAngle == 180)
+            {
+
+                Console.WriteLine();
+
+            }
+            else if (userAngle == 270)
+            {
+
+                Console.WriteLine();
+
+            }
+            else if (userAngle >= 0 && userAngle <= 90)
+            {
+                Console.WriteLine();
+            }
+            else if (userAngle >= 90 && userAngle <= 180)
+            {
+                Console.WriteLine();
+            }
+            else if (userAngle >= 180 && userAngle <= 270)
+            {
+                Console.WriteLine();
+            }
+            else if (userAngle >= 270 && userAngle <= 360)
+            {
+                Console.WriteLine();
+            }
         }
     }
 }
