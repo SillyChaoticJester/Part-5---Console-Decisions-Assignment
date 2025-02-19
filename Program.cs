2fa04f253907d9ee11a3d97f9bb410f0f7b9bf96
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-19 13:47:37 -0500

    Working on Problem Two

--- Program.cs
+++ Program.cs
@@ -5,10 +5,12 @@
         static void Main(string[] args)
         {
             int userAngle, hurricaneSpeed;
-            double minCost, userCost, maxCost;
+            double minCost, userCost, maxCost, userMin;
             minCost = 4.00;
             maxCost = 20.00;
 
+
+            //Problem One
             Console.WriteLine("Type down an angle between 0-360, and I'll tell you the direction");
             userAngle = Convert.ToInt32(Console.ReadLine());
             if (userAngle == 360 || userAngle == 0)
@@ -51,6 +53,22 @@
             {
                 Console.WriteLine("That would be NorthWest!");
             }
+
+            //Problem Two
+            Console.WriteLine("How many minutes were you parked in the garage for?");
+            userMin = Convert.ToDouble(Console.ReadLine());
+
+            if (userMin >= 0 && userMin < 60)
+            {
+                Console.WriteLine($"Your fee is {minCost}");
+            }
+            else if (userMin >= 480)
+            {
+                Console.WriteLine($"Your fee is {maxCost}");
+            }
+
+            //Problem Three
+
         }
     }
 }
